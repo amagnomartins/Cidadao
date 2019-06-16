f68ace6b7f832ca8328b3eccd1311936bea992a3
Alexandre Magno <amagnomartins@icloud.com>
2019-06-16 11:24:00 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/ufabc/cidadao/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.edu.ufabc.cidadao
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
